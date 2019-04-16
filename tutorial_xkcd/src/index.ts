f53237f78afc40e61734b231620f60aaf208d23c
MarcusJones <jones.0bj3@gmail.com>
2019-04-16 17:12:50 +0200

    update tutorial for bookmark menu
    Disabled XKCD plugin!

--- tutorial_xkcd/src/index.ts
+++ tutorial_xkcd/src/index.ts
@@ -1,21 +1,23 @@
 import {
-  JupyterLab, JupyterLabPlugin, ILayoutRestorer
+	JupyterLab, JupyterLabPlugin, ILayoutRestorer
 } from '@jupyterlab/application';
 
 import {
-  ICommandPalette, InstanceTracker
+	IFrame, ICommandPalette, InstanceTracker
 } from '@jupyterlab/apputils';
 
+import { IMainMenu } from '@jupyterlab/mainmenu';
+
 import {
-  JSONExt
+	JSONExt
 } from '@phosphor/coreutils';
 
 import {
-  Message
+	Message
 } from '@phosphor/messaging';
 
 import {
-  Widget
+	Widget, Menu
 } from '@phosphor/widgets';
 
 import '../style/index.css';
@@ -28,92 +30,138 @@ class XkcdWidget extends Widget {
   /**
    * Construct a new xkcd widget.
    */
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.id = 'xkcd-jupyterlab';
-    this.title.label = 'xkcd.com';
-    this.title.closable = true;
-    this.addClass('jp-xkcdWidget');
+		this.id = 'xkcd-jupyterlab';
+		this.title.label = 'xkcd.com';
+		this.title.closable = true;
+		this.addClass('jp-xkcdWidget');
 
-    this.img = document.createElement('img');
-    this.img.className = 'jp-xkcdCartoon';
-    this.node.appendChild(this.img);
+		this.img = document.createElement('img');
+		this.img.className = 'jp-xkcdCartoon';
+		this.node.appendChild(this.img);
 
-    this.img.insertAdjacentHTML('afterend',
-      `<div class="jp-xkcdAttribution">
+		this.img.insertAdjacentHTML('afterend',
+			`<div class="jp-xkcdAttribution">
         <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-xkcdAttribution" target="_blank">
           <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
         </a>
       </div>`
-    );
-  }
+		);
+	}
 
   /**
    * The image element associated with the widget.
    */
-  readonly img: HTMLImageElement;
+	readonly img: HTMLImageElement;
 
   /**
    * Handle update requests for the widget.
    */
-  onUpdateRequest(msg: Message): void {
-    fetch('https://egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
-      return response.json();
-    }).then(data => {
-      this.img.src = data.img;
-      this.img.alt = data.title;
-      this.img.title = data.alt;
-    });
-  }
+	onUpdateRequest(msg: Message): void {
+		fetch('https://egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
+			return response.json();
+		}).then(data => {
+			this.img.src = data.img;
+			this.img.alt = data.title;
+			this.img.title = data.alt;
+		});
+	}
 };
 
 
 /**
  * Activate the xckd widget extension.
  */
-function activate(app: JupyterLab, palette: ICommandPalette, restorer: ILayoutRestorer) {
-  console.log('JupyterLab extension jupyterlab_xkcd is activated!');
+function activate(app: JupyterLab, mainMenu: IMainMenu, palette: ICommandPalette, restorer: ILayoutRestorer) {
+	// console.log('JupyterLab extension jupyterlab_xkcd is activated!');
 
-  // Declare a widget variable
-  let widget: XkcdWidget;
+	// ************** XKCD **************
+	// Declare a widget variable
+	let widget: XkcdWidget;
 
-  // Add an application command
-  const command: string = 'xkcd:open';
-  app.commands.addCommand(command, {
-    label: 'Random xkcd comic',
-    execute: () => {
-      if (!widget) {
-        // Create a new widget if one does not exist
-        widget = new XkcdWidget();
-        widget.update();
-      }
-      if (!tracker.has(widget)) {
-        // Track the state of the widget for later restoration
-        tracker.add(widget);
-      }
-      if (!widget.isAttached) {
-        // Attach the widget to the main work area if it's not there
-        app.shell.addToMainArea(widget);
-      } else {
-        // Refresh the comic in the widget
-        widget.update();
-      }
-      // Activate the widget
-      app.shell.activateById(widget.id);
-    }
-  });
+	// Add an application command
+	// const command: string = 'xkcd:open';
+	// app.commands.addCommand(command, {
+	//   label: 'Random xkcd comic',
+	//   execute: () => {
+	//     if (!widget) {
+	//       // Create a new widget if one does not exist
+	//       widget = new XkcdWidget();
+	//       widget.update();
+	//     }
+	//     if (!tracker.has(widget)) {
+	//       // Track the state of the widget for later restoration
+	//       tracker.add(widget);
+	//     }
+	//     if (!widget.isAttached) {
+	//       // Attach the widget to the main work area if it's not there
+	//       app.shell.addToMainArea(widget);
+	//     } else {
+	//       // Refresh the comic in the widget
+	//       widget.update();
+	//     }
+	//     // Activate the widget
+	//     app.shell.activateById(widget.id);
+	// 	}
 
-  // Add the command to the palette.
-  palette.addItem({ command, category: 'Tutorial' });
+		// // Add the command to the palette.
+		// palette.addItem({ command, category: 'Tutorial' });
 
-  // Track and restore the widget state
-  let tracker = new InstanceTracker<Widget>({ namespace: 'xkcd' });
-  restorer.restore(tracker, {
-    command,
-    args: () => JSONExt.emptyObject,
-    name: () => 'xkcd'
-  });
+		// // Track and restore the widget state
+		// let tracker = new InstanceTracker<Widget>({ namespace: 'xkcd' });
+		// restorer.restore(tracker, {
+		// 	command,
+		// 	args: () => JSONExt.emptyObject,
+		// 	name: () => 'xkcd'
+		// });
+
+	// ******* MENU ITEM *************
+	// Create new commands, append to app.commands
+	function appendNewCommand(item: any) {
+		let iframe: IFrame = null;
+		let command = `BookMark-${item.name}:show`;
+		app.commands.addCommand(command, {
+
+			label: item.name,
+			execute: () => {
+				if (item.target == '_blank') {
+					let win = window.open(item.url, '_blank');
+					win.focus();
+				} else if (item.target == 'widget') {
+					if (!iframe) {
+						iframe = new IFrame();
+						iframe.url = item.url;
+						iframe.id = item.name;
+						iframe.title.label = item.name;
+						iframe.title.closable = true;
+						iframe.node.style.overflowY = 'auto';
+					}
+
+					if (iframe == null || !iframe.isAttached) {
+						app.shell.addToMainArea(iframe);
+						app.shell.activateById(iframe.id);
+					} else {
+						app.shell.activateById(iframe.id);
+					}
+				}
+			}
+
+
+
+		});
+		
+	}
+		BookMarks.forEach(item => appendNewCommand(item));
+
+		console.log('Add bookmarks');
+		
+
+    let menu = Private.createMenu(app);
+    mainMenu.addMenu(menu, {rank: 80});
+    return Promise.resolve(void 0);
+
 };
 
 
@@ -121,10 +169,38 @@ function activate(app: JupyterLab, palette: ICommandPalette, restorer: ILayoutRe
  * Initialization data for the jupyterlab_xkcd extension.
  */
 const extension: JupyterLabPlugin<void> = {
-  id: 'jupyterlab_xkcd',
-  autoStart: true,
-  requires: [ICommandPalette, ILayoutRestorer],
-  activate: activate
+	id: 'jupyterlab_xkcd',
+	autoStart: true,
+	requires: [IMainMenu, ICommandPalette, ILayoutRestorer],
+	activate: activate
 };
 
+
+export const BookMarks = [
+	{
+		name: 'Commons Marketplace',
+		url: 'https://commons.oceanprotocol.com/',
+		description: 'Custom extension to bookmark your MarketPlaces',
+		target: 'widget'
+	}
+];
+
+
 export default extension;
+
+
+namespace Private {
+
+	/**
+	 * Creates a menu for the help plugin.
+	 */
+	export function createMenu(app: JupyterLab): Menu {
+
+		const { commands } = app;
+		let menu: Menu = new Menu({ commands });
+		menu.title.label = 'Market Places';
+		BookMarks.forEach(item => menu.addItem({ command: `BookMark-${item.name}:show` }));
+
+		return menu;
+	}
+}
