aefb4062a5b00e287c0a87a55654fe1db3e582e1
MarcusJones <jones.0bj3@gmail.com>
2019-04-16 16:14:13 +0200

    refactor seperate functions

--- tutorial_xkcd/src/index.ts
+++ tutorial_xkcd/src/index.ts
@@ -2,84 +2,107 @@ import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
-import '../style/index.css';
-
-/* tslint:disable:no-unused-variable */
 import {
   ICommandPalette
 } from '@jupyterlab/apputils';
 
+import {
+  Message
+} from '@phosphor/messaging';
 
 import {
   Widget
 } from '@phosphor/widgets';
 
+import '../style/index.css';
+
+
 /**
- * Initialization data for the tutorial xks extension.
+ * An xckd comic viewer.
  */
-const extension: JupyterLabPlugin<void> = {
-  id: 'tutorial xks',
-  autoStart: true,
-  requires: [ICommandPalette],
-  activate: (app: JupyterLab, palette: ICommandPalette) => {
-    console.log('Updated!');
-    console.log('JupyterLab extension tutorial xks is activated!');
+class XkcdWidget extends Widget {
+  /**
+   * Construct a new xkcd widget.
+   */
+  constructor() {
+    super();
 
-	  // Create a single widget
-	  let widget: Widget = new Widget();
-	  widget.id = 'xkcd-jupyterlab';
-	  widget.title.label = 'xkcd.com';
-	  widget.title.closable = true;
-	  widget.addClass('jp-xkcdWidget'); 
+    this.id = 'xkcd-jupyterlab';
+    this.title.label = 'xkcd.com';
+    this.title.closable = true;
+    this.addClass('jp-xkcdWidget');
 
-	// Add an image element to the panel
-	let img = document.createElement('img');
-	img.className = 'jp-xkcdCartoon'; 
-	widget.node.appendChild(img);
+    this.img = document.createElement('img');
+    this.img.className = 'jp-xkcdCartoon';
+    this.node.appendChild(this.img);
 
-  // New: add an attribution badge
-  img.insertAdjacentHTML('afterend',
-    `<div class="jp-xkcdAttribution">
-      <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-xkcdAttribution" target="_blank">
-        <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
-      </a>
-    </div>`
-  );
+    this.img.insertAdjacentHTML('afterend',
+      `<div class="jp-xkcdAttribution">
+        <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-xkcdAttribution" target="_blank">
+          <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
+        </a>
+      </div>`
+    );
+  }
 
-	// Fetch info about a random comic
-	fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
-	return response.json();
-	}).then(data => {
-	img.src = data.img;
-	img.alt = data.title;
-	img.title = data.alt;
-	});
+  /**
+   * The image element associated with the widget.
+   */
+  readonly img: HTMLImageElement;
 
-	  // Add an application command
-	  const command: string = 'xkcd:open';
-	  app.commands.addCommand(command, {
-	    label: 'Random xkcd comic',
-	    execute: () => {
-	      if (!widget.isAttached) {
-		// Attach the widget to the main work area if it's not there
-		app.shell.addToMainArea(widget);
-	      }
-	      // Activate the widget
-	      app.shell.activateById(widget.id);
-	    }
-	  });
-
-	  // Add the command to the palette.
-	  palette.addItem({command, category: 'Tutorial'});
-
-    console.log('ICommandPalette:', palette);
+  /**
+   * Handle update requests for the widget.
+   */
+  onUpdateRequest(msg: Message): void {
+    fetch('https://egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
+      return response.json();
+    }).then(data => {
+      this.img.src = data.img;
+      this.img.alt = data.title;
+      this.img.title = data.alt;
+    });
   }
 };
 
 
+/**
+ * Activate the xckd widget extension.
+ */
+function activate(app: JupyterLab, palette: ICommandPalette) {
+  console.log('JupyterLab extension jupyterlab_xkcd is activated!');
+
+  // Create a single widget
+  let widget: XkcdWidget = new XkcdWidget();
+
+  // Add an application command
+  const command: string = 'xkcd:open';
+  app.commands.addCommand(command, {
+    label: 'Random xkcd comic',
+    execute: () => {
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        app.shell.addToMainArea(widget);
+      }
+      // Refresh the comic in the widget
+      widget.update();
+      // Activate the widget
+      app.shell.activateById(widget.id);
+    }
+  });
+
+  // Add the command to the palette.
+  palette.addItem({ command, category: 'Tutorial' });
+};
+
+
+/**
+ * Initialization data for the jupyterlab_xkcd extension.
+ */
+const extension: JupyterLabPlugin<void> = {
+  id: 'jupyterlab_xkcd',
+  autoStart: true,
+  requires: [ICommandPalette],
+  activate: activate
+};
 
 export default extension;
-
-// "compilerOptions": {
-//     "noUnusedLocals": false;
-//   },
